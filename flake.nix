37aa23065170a1eda2cf495968d278aea0144e00
Christina O'Donnell <cdo@mutix.org>
2024-11-26 23:35:51 +0000

    Reproduce on a single repo.

--- flake.nix
+++ flake.nix
@@ -4,32 +4,40 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs"; # Use the latest Nixpkgs
     flake-utils.url = "github:numtide/flake-utils"; # Utility functions for flakes
-    lipics.url = "github:cdo256/lipics-authors";
+    #lipics.url = "github:cdo256/lipics-authors";
   };
 
-  outputs = { self, nixpkgs, flake-utils, lipics }: 
+  outputs = { self, nixpkgs, flake-utils }: 
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let 
         pkgs = import nixpkgs { inherit system; };
-        texlive = pkgs.texliveFull.withPackages (ps: [
+        lipicsPkg = pkgs.stdenv.mkDerivation {
+          name = "lipics";
+          pname = "lipics";
+          src = ./lipics;
+          outputs = [ "out" "tex" ];
+          #nativeBuildInputs = [ lipicsTexlive ];
+          installPhase = ''
+            mkdir -p $out/share/texmf/tex/latex/lipics/
+            mkdir -p $tex/tex/latex/lipics/
+            cp *.cls $out/share/texmf/tex/latex/lipics/
+            cp *.cls $tex/tex/latex/lipics/
+          '';
+        };
+        texliveBundle = pkgs.texliveFull.withPackages (ps: [
           ps.latexmk
           #ps.pgf # TikZ
           #pkgs.inkscape
           #ps.luatex
+          lipicsPkg
         ]);
-        fucker = 
-            pkgs.texlive.combine {
-              inherit (pkgs.texlive) scheme-full;
-              inherit (lipics.packages."x86_64-linux") default;
-            };
+        #fucker = 
+        #    pkgs.texlive.combine {
+        #      inherit (pkgs.texlive) scheme-full;
+        #      inherit (lipics.packages."x86_64-linux") default;
+        #    };
       in
       {
-        #packages.devShell = pkgs.stdenv.mkShell {
-        #  nativeBuildInputs = [ 
-        #    pkgs.gnumake
-        #    texlive
-        #  ];
-        #};
         packages.default = pkgs.stdenv.mkDerivation {
           name = "build-paper";
           src = ./.;
@@ -38,17 +46,11 @@
             #texlive
             ##pkgs.texlivePackages.latexmk
 	    #lipics.packages.${system}.default
-            fucker
+            texliveBundle
           ];
 
-          buildInputs = [
-            #texlive
-            #pkgs.texlivePackages.latexmk
-          ];
-         
           buildPhase = ''
             export XDG_CACHE_HOME=$(mktemp -d)
-            echo $PATH
             latexmk -pdf --shell-escape paper.tex
           '';
 
